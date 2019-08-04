4fbbb4e79b181c8532b5ccae16a6f434795be849
Sachitha Ilayperuma <49128826+sachitha-ilayperuma@users.noreply.github.com>
2019-08-04 18:57:47 +0530

    orders

--- src/app/dashboard/orders/orders.component.ts
+++ src/app/dashboard/orders/orders.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/services/auth-service.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSmartModalService } from 'ngx-smart-modal';
+import {OrdersService} from '../../services/orders.service';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
-
-  constructor() { }
+  allOrders: any = [];
+  searchText = null;
+  showSpinner =  true;
+  dataSaved = false;
+  messaage = null;
+  constructor(
+    private ordersService: OrdersService,
+    private authService: AuthService,
+    private toastCtrl: ToastrService,
+    public ngxSmartModalService: NgxSmartModalService
+  ) { }
 
   ngOnInit() {
+    this.loadAllOrders();
+  }
+  logOut() {
+    this.authService.logout();
+    console.log('Logout');
+  }
+  loadAllOrders() {
+    this.ordersService.getAllOrders().subscribe( res => {
+      this.allOrders = res;
+      this.showSpinner = false;
+      console.log(this.allOrders);
+    },
+    error => {
+      console.log(error);
+      if (error.status === 0) {
+        alert('Connection Error');
+      }
+    }
+    );
+  }
+  resetTable() {
+    this.messaage = null;
+    this.dataSaved = false;
   }
 
+  search(ev: any) {
+    console.log(ev.target.value);
+    this.searchText = ev.target.value;
+
+    console.log(this.allOrders);
+  }
 }
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -8,8 +8,10 @@ export class FilterPipe implements PipeTransform {
     if (!searchText) return items;
     searchText = searchText.toLowerCase();
     return items.filter(it => {
-      return ((it.product_name .toLowerCase().includes(searchText)) || (it.category.toLowerCase().includes(searchText))
-       || (it.sub_category.toLowerCase().includes(searchText)));
+      return ((it.product_name .toLowerCase().includes(searchText)) 
+      || (it.category.toLowerCase().includes(searchText))
+       || (it.sub_category.toLowerCase().includes(searchText))
+       );
     });
   }
 }
